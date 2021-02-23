f2dd282aea118d73b61e1f5424c454bf877324f5
paschoal <jaqueline.paschoal@scaffoldeducation.com.br>
2021-02-23 18:11:14 -0300

    init priject

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,12 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 px</span>
+      <div>
+        <div style={{ width: '60%'}}/>
+        <span style={{ left: '60%'}} className="current-experience">300xp</span>
+      </div>
+      <span>600px</span>
+    </header>
+  )
+}
